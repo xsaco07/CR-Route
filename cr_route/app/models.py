790987622f5b30b83d135422924e6dfcd0fff7b1
Emmanuel Alfaro Brenes <ealfarobrenes@hotmail.com>
2019-10-24 12:48:44 -0600

    modelos de la DB

---
+++ cr_route/app/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Empresa(models.Model):
+    nombre = models.CharField(max_length=30)
+    zona = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=30)
+    correo = models.EmailField()
+    direccion = models.CharField(max_length=30)
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+    horario = models.TextField()
+
+class Ruta(models.Model):
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=200)
+    precio = models.IntegerField()
+    horario = models.TextField()
+    duracion = models.IntegerField()
+    rampa = models.BooleanField()
+
+class Parada(models.Model):
+    ruta = models.ForeignKey(Ruta, on_delete=models.CASCADE)
+    serial = models.IntegerField()
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+
+class Usuario(models.Model):
+    nombre_usuario = models.CharField(max_length=30)
+    apellido1 = models.CharField(max_length=30)
+    apellido2 = models.CharField(max_length=30)
+    contrasena = models.CharField(max_length=30)
+
+class Log(models.Model):
+    nombre_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    accion = models.CharField(max_length=30)
+    tabla = models.CharField(max_length=30)
+    hora = models.TimeField(auto_now=True) # don't need to pass this value to constructor
