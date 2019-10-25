18a670f3dccfbc548946d0757b72190a8352219e
isaac.mena07 <xsaco07@gmail.com>
2019-10-24 19:27:04 -0600

    dynamic deployment of routes int the html file

--- cr_route/app/views.py
+++ cr_route/app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from app.models import *
+
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def admin_rutas(request):
+    rutas = Ruta.objects.all()
+    print(rutas)
+    return render(request, 'admRutas.html', {'rutas':rutas})
--- cr_route/cr_route/urls.py
+++ cr_route/cr_route/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home),
+    path('admin_rutas/', admin_rutas, name='adm_rutas'),
 ]
