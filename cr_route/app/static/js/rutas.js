d19b771cd2ec1083d7f1a341e48eb3f3fd9cf15a
isaac.mena07 <xsaco07@gmail.com>
2019-11-20 02:27:21 -0600

    Build json object with all markers in the map and finish stop ui to add description

--- cr_route/app/static/js/rutas.js
+++ cr_route/app/static/js/rutas.js
@@ -6,9 +6,11 @@ var startPoint = {
 var markers = [];
 var coordenates = [];
 var polylines = []
+var descriptions = [];
 var mymap = L.map('map').setView([startPoint.latitude, startPoint.longitude], 16);
 var p_group = L.layerGroup(polylines).addTo(mymap);
 var stop_flag = false;
+const stop_form = "input"
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -27,29 +29,120 @@ function onMapClick(e) {
 
   var newMarker = null;
 
+  // Is a stop?
   if(!stop_flag){
+
     newMarker = L.marker(e.latlng);
+    newMarker.addTo(mymap);
+
+    // Add function on click event
+    newMarker.on('click',function(){
+      remove_marker(this);
+    });
+
   }
+
   else{
+
     newMarker = L.marker(e.latlng, {icon: greenIcon});
+    newMarker.addTo(mymap);
+
+    // Always deploy the form when is added to the map
+    deploy_stop_form(newMarker)
+
+    newMarker.on('click', function() {
+      deploy_stop_form(newMarker);
+    });
+
   }
 
   var lat = e.latlng.lat;
   var lng = e.latlng.lng;
 
-  newMarker.addTo(mymap);
-
-  newMarker.on('click',function(){
-
-    remove_marker(this);
-
-  });
-
+  // Save marker in array
+  // The description is empty unless the user intentionally fills it
   markers.push(newMarker);
+  // Save new coordenates in array
   coordenates.push([lat, lng]);
 
   draw_path(coordenates);
 
+}
+
+// Shows the delete and add description options to the stop marker
+// Add event handlers to both buttons
+function deploy_stop_form(marker) {
+
+  // Bind html to popup to make an markers_function
+  // Add a description or delete it
+  var options_html = "<h6 class='text-center'>Agregando Parada</h6> \
+  <input type='button' class='btn btn-success' value='Describir' id='desc-btn-option'> \
+  <input type='button' class='btn btn-danger' value='Eliminar' id='delete-btn-option'>";
+
+  marker.bindPopup(options_html).openPopup();
+
+  // Bind html to popup to add a description
+  var add_desc_html = "<h5>Agregue una descripción</h5> \
+  <div class='form-group'> \
+  <input class='form-control' id='desc-input' type='text'> \
+  </div> \
+  <div class='text-center'> \
+  <input type='button' class='btn btn-success' value='Listo' class='text-center' id='desc-btn'> \
+  </div>";
+
+  $('#desc-btn-option').click(function() {
+
+    marker.bindPopup(add_desc_html).openPopup();
+
+    // Save the descripction when btn is pressed
+    $("#desc-btn").click(function() {
+      var current_stop_description = $("#desc-input").val();
+      var desc_position = markers.indexOf(marker);
+      descriptions[desc_position] = current_stop_description;
+      marker.closePopup();
+    });
+
+  });
+
+  $('#delete-btn-option').click(function() {
+    remove_marker(marker);
+  });
+
+}
+
+// Takes the attributes from coords and markers array and
+// turns it to an json object.
+// marker: object where the attributes are taken
+// is_stop: true if stop_flag was true when it was added to the map
+// is_stop: false if stop_flag was false when it was added to the map
+function marker_to_json(marker) {
+
+  // If the description is not set means that is not a stop
+  var is_stop = false;
+  var description = "";
+  if(descriptions[markers.indexOf(marker)] != null){
+    is_stop = true;
+    description = descriptions[markers.indexOf(marker)];
+  }
+
+  var json_object = {
+    "latitud" : marker.getLatLng().lat,
+    "longitud" : marker.getLatLng().lng,
+    "esParada" : is_stop,
+    "descripcion" : description
+  };
+
+  return json_object;
+}
+
+function get_all_points_json() {
+  var current_serial = 0;
+  var points = {};
+  for(var i = 0; i < markers.length; i++) {
+    points[(current_serial++).toString()] = marker_to_json(markers[i]);
+  }
+  console.log(points);
+  return points;
 }
 
 function remove_marker(marker) {
@@ -69,7 +162,7 @@ function remove_marker(marker) {
 
 }
 
-// This function is called the page is loaded with the purpose of updating a route
+// This function is called when the page is loaded with the purpose of updating a route
 // It takes the points from DB and draws the path through out the map
 function draw_loaded_path() {
   markers = [];
@@ -106,8 +199,9 @@ function cleanMap() {
   coordenates = [];
   p_group.remove();
   p_group = L.layerGroup(polylines).addTo(mymap);
+
+  // Apply remove function to every marker in array
   markers.map(function(marker) {
-    console.log("Cleaning marker");
     marker.remove();
   });
 }
@@ -133,6 +227,7 @@ $(document).ready(function() {
   submit_button.on('click', function() {
     var hidden_input = $('#puntos-ruta');
     hidden_input.val(coordenates.toString());
+    console.log(get_all_points_json());
   });
 
 });
