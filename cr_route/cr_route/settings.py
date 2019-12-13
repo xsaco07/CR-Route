9de876180c257f2ff9652460c4c71c779a2cba7f
isaac.mena07 <xsaco07@gmail.com>
2019-12-13 14:09:20 -0600

    Add django-heroku package

--- cr_route/cr_route/settings.py
+++ cr_route/cr_route/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/cr_route/app/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
